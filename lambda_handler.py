7b7dcf3c000cffb512f8976dc5287e4c6020c6ae
Matthew Salazar <matthew@mtslzr.io>
2017-08-01 13:49:25 -0500

    Changed functions to a class; removed duplicate array setting

--- lambda_handler.py
+++ lambda_handler.py
@@ -2,98 +2,101 @@ import boto3
 import time
 from datetime import datetime
 
-# Main Handler
+# Function - Main Handler
 def handler(event, context):
-    # If ID, we're getting an existing run.
-    if 'id' in event['params']['path']:
-    	return getFizzbuzz(event['params']['path']['id'])
-    # Else, we're creating a new entry.
-    else:
-        # Setup data and ensure we're using numbers.
-        data = {}
-        for key,val in event['params']['path'].iteritems():
-            try:
-                int(val)
-                data[key] = int(val)
-            except:
-                return val + " is not an Integer."
+	# Start our class.
+	fb = Fizzbuzz()
 
-        if data['x'] == 0 or data['y'] == 0:
-        	return "Stop trying to divide by zero. :("
+	# If ID, we're getting an existing run.
+	if 'id' in event['params']['path']:
+		return fb.getFizzbuzz(event['params']['path']['id'])
+	# Else, we're creating a new entry.
+	else:
+		# Setup data and ensure we're using numbers.
+		data = {}
+		for key,val in event['params']['path'].iteritems():
+			try:
+				int(val)
+				data[key] = int(val)
+			except:
+				return val + " is not an Integer."
 
-        # Set default data, if needed. 
-        if len(data) == 0:
-            data = { 'x': 3, 'y': 5 }
-        if not 'z' in data:
-            data['z'] = 100
+		if data['x'] == 0 or data['y'] == 0:
+			return "Stop trying to divide by zero. :("
 
-        return runFizzbuzz(data)
+		# Set default data, if needed. 
+		if len(data) == 0:
+			data = { 'x': 3, 'y': 5 }
+		if not 'z' in data:
+			data['z'] = 100
 
-# Connect to Dynamo.
-def connect():
-    session = boto3.Session()
-    dynamodb = session.resource('dynamodb')
-    return dynamodb.Table('fizzbuzz')
+		return fb.runFizzbuzz(data)
 
-def getFizzbuzz(id):
-    # Connect to Dynamo.
-    fb = connect()
+# Fizzbuzz Class
+class Fizzbuzz:
+	# Function - Connect to Dynamo.
+	def connect(self):
+	    session = boto3.Session()
+	    dynamodb = session.resource('dynamodb')
+	    return dynamodb.Table('fizzbuzz')
 
-    # Pull entry from Dynamo for that ID.
-    response = fb.get_item(
-        Key={ 'id': id }
-    )
-    if 'Item' in response:
-        return response['Item']
-    else:
-        return 'Sorry, no entry matches that ID.'
+	# Function - Search DynamoDB for previous Fizzbuzz ID.
+	def getFizzbuzz(self, id):
+	    # Connect to Dynamo.
+	    fb = self.connect()
 
-def runFizzbuzz(data):
-	# Set timestamp.
-	currentTime = int(time.time())
-	# Create a list of matching numbers.
-	matches = []
+	    # Pull entry from Dynamo for that ID.
+	    response = fb.get_item(
+	        Key={ 'id': id }
+	    )
+	    if 'Item' in response:
+	        return response['Item']
+	    else:
+	        return 'Sorry, no entry matches that ID.'
 
-	# Loop through our number range.
-	for z in range(1,(data['z'] + 1)):
-		# If divisible by X and Y, add to fizzbuzz.
-		if z % data['x'] == 0 and z % data['y'] == 0:
-			matches.append(z)
+	# Function - Process new Fizzbuzz entry from variables given.
+	def runFizzbuzz(self, data):
+		# Set timestamp.
+		currentTime = int(time.time())
+		# Create a list of matching numbers.
+		matches = []
 
-	# Store our data in Dynamo
-	response = saveFizzbuzz(fizzbuzzCount, matches, data, currentTime)
-	if response:
-		out = {
+		# Loop through our number range.
+		for z in range(1,(data['z'] + 1)):
+			# If divisible by X and Y, add to fizzbuzz.
+			if z % data['x'] == 0 and z % data['y'] == 0:
+				matches.append(z)
+
+		# Store our data in Dynamo
+		response = self.saveFizzbuzz(matches, data, currentTime)
+		if 'id' in response:
+			# Remove ID and add URL for user.
+			response.pop('id')
+			response['url'] = 'https://devops-fizzbuzz.mtslzr.io/id/' + str(currentTime)
+			return response
+		else:
+			# Return error response from Dynamo.
+			return response
+
+	# Function - Store Fizzbuzz data to DynamoDB.
+	def saveFizzbuzz(self, matches, data, currentTime):
+		# Connect to Dynamo.
+		fb = self.connect()
+
+		# Set up data to be written.
+		payload = {
+			'id': str(currentTime),
 			'x': data['x'],
 			'y': data['y'],
 			'z': data['z'],
-			'fizzbuzzCount': count(matches),
+			'fizzbuzzCount': len(matches),
 			'matches': matches,
 			'timestamp': datetime.fromtimestamp(currentTime).strftime('%Y-%m-%d %H:%M:%S'),
-			'url': 'https://devops-fizzbuzz.mtslzr.io/id/' + str(currentTime)
 		}
-		return out
-	else:
-		return response
-	
-def saveFizzbuzz(fizzbuzzCount, matches, data, currentTime):
-	# Connect to Dynamo.
-	fb = connect()
 
-	# Set up data to be written.
-	payload = {
-		'id': str(currentTime),
-		'x': data['x'],
-		'y': data['y'],
-		'z': data['z'],
-		'fizzbuzzCount': count(matches),
-		'matches': matches,
-		'timestamp': datetime.fromtimestamp(currentTime).strftime('%Y-%m-%d %H:%M:%S'),
-	}
-
-	# Write to database; if successful return True. Otherwise, return the response info.
-	response = fb.put_item(Item=payload)
-	if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-		return True
-	else:
-		return response
+		# Write to database; if successful return True. Otherwise, return the response info.
+		response = fb.put_item(Item=payload)
+		if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+			return payload
+		else:
+			return response
